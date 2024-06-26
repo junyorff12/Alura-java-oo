2c6e52cf3a972006b0a03c560a3bab8f243775e0
junyorff12 <junyorff12@gmail.com>
2024-06-25 23:04:00 -0300

    Trabalhand com JavaDoc

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
@@ -1,5 +1,12 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe que representa um cliente no bytebank
+ * 
+ * @author JRFF12
+ * @version 0.1
+ */
+
 public class Cliente {
 
     private String nome;
--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,12 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe representa uma moldura de uma conta.
+ * 
+ * @author JRFF12
+ * @version 0.1
+ * 
+ */
 
 public abstract class Conta {
 
@@ -9,6 +16,13 @@ public abstract class Conta {
     private Cliente titular;
     private static int total = 0;
     
+    
+    /**
+     * Construtor para inicializar um conta apartir de uma agencia e de uma numero.
+     * 
+     * @param agencia
+     * @param numero
+     */
     public Conta(int agencia, int numero){
         Conta.total++;
         //System.out.println("O total de contas é " + Conta.total);
@@ -19,7 +33,15 @@ public abstract class Conta {
     }
 
     public abstract void deposita(double valor);
-
+    
+    
+    /**
+     * 
+     * Valor precisa ser maior que o saldo.
+     * 
+     * @param valor
+     * @throws SaldoInsuficienteException
+     */
     public void saca(double valor) throws SaldoInsuficienteException{
     	
         if(this.saldo < valor) {
