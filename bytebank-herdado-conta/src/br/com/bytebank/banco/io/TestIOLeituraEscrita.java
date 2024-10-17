441b0edf852ff61645131147d9699524ce54acc1
junyorff12 <junyorff12@gmail.com>
2024-10-16 22:52:31 -0300

    lendo arquivo

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/io/TestIOLeituraEscrita.java
@@ -0,0 +1,26 @@
+package br.com.bytebank.banco.io;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestIOLeituraEscrita {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis = new FileInputStream("loren.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		
+		String line = br.readLine();
+		while(line != null) {
+			System.out.println(line);
+			line = br.readLine();
+		}
+		
+		br.close();
+	}
+
+}
