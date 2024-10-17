2c658754a1dfcd03cf3d09fb7a245cde9fe460af
junyorff12 <junyorff12@gmail.com>
2024-10-16 23:07:16 -0300

    trabalhando com input e output do telcado e console

--- bytebank-herdado-conta/src/br/com/bytebank/banco/io/TestIOLeituraEscritaSystemIn.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/io/TestIOLeituraEscritaSystemIn.java
@@ -7,16 +7,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public class TestIOLeituraEscrita {
+public class TestIOLeituraEscritaSystemIn {
 
 	public static void main(String[] args) throws IOException {
 		
-		FileInputStream fis = new FileInputStream("loren.txt");
+		InputStream fis = System.in;
 		InputStreamReader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 		
-		FileOutputStream fos = new FileOutputStream("loren2.txt");
+		OutputStream fos = System.out;//new FileOutputStream("loren2.txt");
 		OutputStreamWriter osr = new OutputStreamWriter(fos);
 		BufferedWriter bw = new BufferedWriter(osr);
 		
