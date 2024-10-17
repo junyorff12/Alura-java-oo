4f76eaa925551447e6d0c5dbe18bd95e269dcbd1
junyorff12 <junyorff12@gmail.com>
2024-10-16 22:59:32 -0300

    lendo e escrevendo

--- bytebank-herdado-conta/src/br/com/bytebank/banco/io/TestIOLeituraEscrita.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/io/TestIOLeituraEscrita.java
@@ -1,9 +1,12 @@
 package br.com.bytebank.banco.io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class TestIOLeituraEscrita {
 
@@ -13,13 +16,19 @@ public class TestIOLeituraEscrita {
 		InputStreamReader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 		
+		FileOutputStream fos = new FileOutputStream("loren2.txt");
+		OutputStreamWriter osr = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osr);
+		
 		
 		String line = br.readLine();
 		while(line != null) {
-			System.out.println(line);
+			bw.write(line);
+			bw.newLine();
 			line = br.readLine();
 		}
 		
+		bw.close();
 		br.close();
 	}
 
